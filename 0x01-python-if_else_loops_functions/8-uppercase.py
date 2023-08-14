eb116d30e4cbcdde448faa3cd045f24f713cffaa
Praisesomiji <praisesomiji@gmail.com>
2023-08-14 02:29:05 +0100

    PSET 8: Fix typoo

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     strlen = len(str)
     for i in range(0, strlen):
-        n = ord(str[1])
+        n = ord(str[i])
         # if character is in lower case
         if n in range(97, 123):
             n = n - 32
