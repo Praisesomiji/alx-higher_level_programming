fc5325c8e933f47b91b0307d949abcb66e1d560c
Praisesomiji <praisesomiji@gmail.com>
2023-08-15 06:20:19 +0100

    PSET 2: Fix bugs

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,7 @@
 def print_reversed_list_integer(my_list=[]):
     i = len(my_list) - 1
 
-    while (i--):
-        print("{:d}.format(i))
+    while (i):
+       print("{:d}".format(i))
+       i -= 1
+print_reversed_list_integer([0, 1, 2, 3])
